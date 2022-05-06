d5e84de3a6f1315464c8248fa0f7b422cedb42f4
gkq1 <gaokeqian97@gmail.com>
2022-05-06 11:03:34 +0800

    Initial commit

---
+++ src/main/java/com/imooc/miaosha/controller/MiaoshaController.java
@@ -0,0 +1,172 @@
+package com.imooc.miaosha.controller;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.imooc.miaosha.domain.MiaoshaOrder;
+import com.imooc.miaosha.domain.MiaoshaUser;
+import com.imooc.miaosha.rabbitmq.MQSender;
+import com.imooc.miaosha.rabbitmq.MiaoshaMessage;
+import com.imooc.miaosha.redis.GoodsKey;
+import com.imooc.miaosha.redis.MiaoshaKey;
+import com.imooc.miaosha.redis.OrderKey;
+import com.imooc.miaosha.redis.RedisService;
+import com.imooc.miaosha.result.CodeMsg;
+import com.imooc.miaosha.result.Result;
+import com.imooc.miaosha.service.GoodsService;
+import com.imooc.miaosha.service.MiaoshaService;
+import com.imooc.miaosha.service.MiaoshaUserService;
+import com.imooc.miaosha.service.OrderService;
+import com.imooc.miaosha.vo.GoodsVo;
+
+@Controller
+@RequestMapping("/miaosha")
+public class MiaoshaController implements InitializingBean {
+
+	@Autowired
+	MiaoshaUserService userService;
+	
+	@Autowired
+	RedisService redisService;
+	
+	@Autowired
+	GoodsService goodsService;
+	
+	@Autowired
+	OrderService orderService;
+	
+	@Autowired
+	MiaoshaService miaoshaService;
+	
+	@Autowired
+	MQSender sender;
+	
+	private HashMap<Long, Boolean> localOverMap =  new HashMap<Long, Boolean>();
+	
+	/**
+	 * 系统初始化
+	 * */
+	public void afterPropertiesSet() throws Exception {
+		List<GoodsVo> goodsList = goodsService.listGoodsVo();
+		if(goodsList == null) {
+			return;
+		}
+		for(GoodsVo goods : goodsList) {
+			redisService.set(GoodsKey.getMiaoshaGoodsStock, ""+goods.getId(), goods.getStockCount());
+			localOverMap.put(goods.getId(), false);
+		}
+	}
+	
+	@RequestMapping(value="/reset", method=RequestMethod.GET)
+    @ResponseBody
+    public Result<Boolean> reset(Model model) {
+		List<GoodsVo> goodsList = goodsService.listGoodsVo();
+		for(GoodsVo goods : goodsList) {
+			goods.setStockCount(10);
+			redisService.set(GoodsKey.getMiaoshaGoodsStock, ""+goods.getId(), 10);
+			localOverMap.put(goods.getId(), false);
+		}
+		redisService.delete(OrderKey.getMiaoshaOrderByUidGid);
+		redisService.delete(MiaoshaKey.isGoodsOver);
+		miaoshaService.reset(goodsList);
+		return Result.success(true);
+	}
+	
+	/**
+	 * QPS:1306
+	 * 5000 * 10
+	 * QPS: 2114
+	 * */
+    @RequestMapping(value="/do_miaosha", method=RequestMethod.POST)
+    @ResponseBody
+    public Result<Integer> miaosha(Model model,MiaoshaUser user,
+    		@RequestParam("goodsId")long goodsId) {
+    	model.addAttribute("user", user);
+    	if(user == null) {
+    		return Result.error(CodeMsg.SESSION_ERROR);
+    	}
+    	//内存标记，减少redis访问
+    	boolean over = localOverMap.get(goodsId);
+    	if(over) {
+    		return Result.error(CodeMsg.MIAO_SHA_OVER);
+    	}
+    	//预减库存
+    	long stock = redisService.decr(GoodsKey.getMiaoshaGoodsStock, ""+goodsId);//10
+    	if(stock < 0) {
+    		 localOverMap.put(goodsId, true);
+    		return Result.error(CodeMsg.MIAO_SHA_OVER);
+    	}
+    	//判断是否已经秒杀到了
+    	MiaoshaOrder order = orderService.getMiaoshaOrderByUserIdGoodsId(user.getId(), goodsId);
+    	if(order != null) {
+    		return Result.error(CodeMsg.REPEATE_MIAOSHA);
+    	}
+    	//入队
+    	MiaoshaMessage mm = new MiaoshaMessage();
+    	mm.setUser(user);
+    	mm.setGoodsId(goodsId);
+    	sender.sendMiaoshaMessage(mm);
+    	return Result.success(0);//排队中
+
+
+
+		//原始逻辑
+		//首先查看商品库存，如果库存大于0继续
+		//然后判断该用户是否已经拍到过这个产品，如果成功过就报错，否则继续
+		//然后尝试减少库存
+
+		//以下代码会并发执行
+
+
+		//重复秒杀问题，也就是一个用户拍到了两个商品
+		//解决方法，加上唯一索引，用户id和商品id
+
+		//提供QPS的方法
+		//1,生成订单后，将数据存入redis, 之后在判断是否该用户是否下单时，采用查询redis的方式
+		//2,
+
+    	/*
+    	//判断库存
+    	GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);//10个商品，req1 req2
+    	int stock = goods.getStockCount();
+    	if(stock <= 0) {
+    		return Result.error(CodeMsg.MIAO_SHA_OVER);
+    	}
+    	//判断是否已经秒杀到了
+    	MiaoshaOrder order = orderService.getMiaoshaOrderByUserIdGoodsId(user.getId(), goodsId);
+    	if(order != null) {
+    		return Result.error(CodeMsg.REPEATE_MIAOSHA);
+    	}
+    	//减库存 下订单 写入秒杀订单
+    	OrderInfo orderInfo = miaoshaService.miaosha(user, goods);
+        return Result.success(orderInfo);
+        */
+    }
+    
+    /**
+     * orderId：成功
+     * -1：秒杀失败
+     * 0： 排队中
+     * */
+    @RequestMapping(value="/result", method=RequestMethod.GET)
+    @ResponseBody
+    public Result<Long> miaoshaResult(Model model,MiaoshaUser user,
+    		@RequestParam("goodsId")long goodsId) {
+    	model.addAttribute("user", user);
+    	if(user == null) {
+			return Result.error(CodeMsg.SESSION_ERROR);
+		}
+    	long result  =miaoshaService.getMiaoshaResult(user.getId(), goodsId);
+    	return Result.success(result);
+    }
+
+}
